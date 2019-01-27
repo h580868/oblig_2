8d7cc39345d64b2572350b80794364e6e92d76c8
hbs <hbs@hvl-300277.hvl.no>
2019-01-27 18:39:43 +0100

    v02

--- MengdeU/src/no/hvl/dat102/mengde/klient/Ordliste.java
+++ MengdeU/src/no/hvl/dat102/mengde/klient/Ordliste.java
@@ -14,7 +14,7 @@ public class Ordliste {
 
 		MengdeADT<String> ordListe1 = new KjedetMengde<String>();
 
-		String[] ord = { "God", "dag", "Hans", "Hansen", "Hansaby", "Olsen", "Ole", "buss", "rute", "Bergen" };
+		String[] ord = { "God", "Førde", "Hans", "Hansen", "Hansaby", "Olsen", "Ole", "buss", "rute", "Bergen" };
 
 		Scanner tastatur = new Scanner(System.in);
 		// Legger til ordene i mengden ordListe1
